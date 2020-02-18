30eac6d2e532fbe2c52a17f6914f1e5c8fcf063c
Paul.py <54999877+Paul-py07@users.noreply.github.com>
2020-02-18 09:08:05 -0600

    Update bugs.js
    Check it out and tell me what you think.

--- bugs.js
+++ bugs.js
@@ -23,3 +23,29 @@ else {
     console.log('Time to play with my pet');
 }
 
+//Paul Ukaegbu--> My version of your code. I'll also try to do the "pullrequest" thing.
+
+var firstName = "Robert";
+var nickName = "Bob";
+var petName = 'Fluffy';
+var petType = "Turtle";
+
+
+if (firstName == "Robert") {
+    console.log("Hi Robert");
+}
+if(nickName == "Bob"){
+    console.log("Hi Bob");
+}
+else if (firstName != "Robert") 
+    { console.log("Hello Stranger")
+}
+if (petType == "Dog") {
+    console.log("Time to play fetch");
+}
+if(petType == "Cat"){
+    console.log("Time to play with a laser pointer");
+}
+else {
+    console.log('Time to play with my pet');
+}
